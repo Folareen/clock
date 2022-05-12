61849142ff8ee94c7fb95835b5bcc258c0bd3cc1
Folareen <sakawahab03@gmail.com>
2022-05-12 22:01:00 +0100

    added clock toggle button

--- home.js
+++ home.js
@@ -1,9 +1,16 @@
 const THEME_BTN = document.querySelector('.theme--btn');
 const TOGGLE = document.querySelector('.toggle');
 const BODY = document.body;
+const CLOCK_BTN = document.querySelector('.clock--btn');
 
 THEME_BTN.addEventListener('click', ()=>{
     TOGGLE.classList.toggle('current');
-    THEME_BTN.classList.toggle('bg');
-    BODY.classList.toggle('body-bg')
+    // THEME_BTN.classList.toggle('bg');
+    BODY.classList.toggle('dark');
+})
+
+CLOCK_BTN.addEventListener('click', ()=>{
+    CLOCK_BTN.children[0].classList.toggle('current--clock');
+    CLOCK_BTN.children[1].classList.toggle('current--clock');
+    // console.log(CLOCK_BTN.children[0])
 })
